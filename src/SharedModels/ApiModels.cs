33cbc5cd19e3ba3be02cd28b104fe9d04e92fdd8
Joshua Ryder <josh@topswagcode.com>
2022-11-12 22:06:08 +0100

    make monorepo solution file and remove warnings from project. No nullpointers today my friend HAHA!

--- src/SharedModels/ApiModels.cs
+++ src/SharedModels/ApiModels.cs
@@ -1,6 +1,6 @@
 ﻿namespace SharedModels
 {
-    public record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+    public record WeatherForecast(DateTime Date, int TemperatureC, string Summary)
     {
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
