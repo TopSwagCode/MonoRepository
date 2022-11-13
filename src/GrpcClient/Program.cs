a6075928819ea61b7aa8d8cf1a1eccee67073f1d
Joshua Ryder <josh@topswagcode.com>
2022-11-13 22:34:16 +0100

    GRPC INCOMMING!!! TAKE COVER!

---
+++ src/GrpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+using var channel = GrpcChannel.ForAddress("https://grpctopswagcode.azurewebsites.net");//"https://grpc.topswagcode.com");
+
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+     new HelloRequest { Name = "TopSwagCode" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
