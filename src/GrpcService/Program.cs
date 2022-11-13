2b1b2c69969e703ef4ee3a8319a2a33af7f50448
Joshua Ryder <josh@topswagcode.com>
2022-11-13 22:45:05 +0100

    Add GRPC specific stuff

--- src/GrpcService/Program.cs
+++ src/GrpcService/Program.cs
@@ -2,6 +2,16 @@ using GrpcService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Kestrel to listen on a specific HTTP port 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+    options.ListenAnyIP(8585, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+    });
+});
+
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
